dcbab8618409fc927a2359296038187b0e03e02b
renatofas <recalvo@alumnos.uai.cl>
2025-05-25 13:27:57 -0400

    perro salchicha

--- front/src/data/products.js
+++ front/src/data/products.js
@@ -36,7 +36,22 @@ const products = [
       images: [
         'https://res.cloudinary.com/dhvxcx2d2/image/upload/f_auto,q_auto,w_600/v1746124987/WhatsApp_Image_2025-05-01_at_14.26.33_3_ew9eot.jpg'
       ]
-    }
+    },
+    {
+      id: '5',
+      name: 'Perro salchicha (20x14 cm)',
+      price: 29.000,
+      description: 'Figura de cerámica pintada a mano con tonos suaves.',
+      images: [
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193800/WhatsApp_Image_2025-05-23_at_21.49.04_izlr0a.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193800/WhatsApp_Image_2025-05-22_at_13.54.11_1_rt6pyj.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193800/WhatsApp_Image_2025-05-22_at_13.54.11_yzfkvv.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193800/WhatsApp_Image_2025-05-22_at_13.54.12_1_cwn5ga.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193803/WhatsApp_Image_2025-05-23_at_13.06.21_2_mueo8c.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193804/WhatsApp_Image_2025-05-23_at_13.06.21_jn6wtg.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193803/WhatsApp_Image_2025-05-23_at_13.06.21_1_wouiuu.jpg'
+      ]
+    },
   ];
   
   export default products;
