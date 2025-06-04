8a87b5003430d54b4161d005ff1b9930795443bf
renatofas <recalvo@alumnos.uai.cl>
2025-06-03 22:44:37 -0400

    final

--- front/src/components/ImageCarousel.js
+++ front/src/components/ImageCarousel.js
@@ -9,13 +9,37 @@ const ImageCarousel = () => {
     <div className="carousel-image">
       <Carousel autoPlay infiniteLoop showThumbs={false}>
         <div>
-          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1746124987/WhatsApp_Image_2025-05-01_at_14.26.30_n7cepp.jpg" alt="Figura de cerámica colorida 1" />
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003696/WhatsApp_Image_2025-05-31_at_14.55.53_qx8kjx.jpg" alt="Figura de cerámica colorida 1" />
         </div>
         <div>
-          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1746124987/WhatsApp_Image_2025-05-01_at_14.26.30_n7cepp.jpg" alt="Figura de cerámica colorida 2" />
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003596/WhatsApp_Image_2025-06-01_at_20.34.59_kbpyjt.jpg" alt="Figura de cerámica colorida 2" />
         </div>
         <div>
-          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1746124987/WhatsApp_Image_2025-05-01_at_14.26.30_n7cepp.jpg" alt="Figura de cerámica colorida 3" />
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003595/WhatsApp_Image_2025-06-01_at_20.34.59_1_fjpnpq.jpg" alt="Figura de cerámica colorida 3" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003594/WhatsApp_Image_2025-06-01_at_20.34.58_2_resggk.jpg" alt="Figura de cerámica colorida 1" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003595/WhatsApp_Image_2025-06-01_at_20.34.58_ezgkjg.jpg" alt="Figura de cerámica colorida 1" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003595/WhatsApp_Image_2025-06-01_at_20.34.59_3_ssoiy1.jpg" alt="Figura de cerámica colorida 1" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003595/WhatsApp_Image_2025-06-01_at_20.35.00_1_jft58g.jpg" alt="Figura de cerámica colorida 1" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003594/WhatsApp_Image_2025-06-01_at_20.35.00_garwdv.jpg" alt="Figura de cerámica colorida 1" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003595/WhatsApp_Image_2025-06-01_at_20.34.58_1_kx50ys.jpg" alt="Figura de cerámica colorida 1" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003594/WhatsApp_Image_2025-06-01_at_20.34.58_3_yl8k39.jpg" alt="Figura de cerámica colorida 1" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003594/WhatsApp_Image_2025-06-01_at_20.34.59_2_tjmgbr.jpg" alt="Figura de cerámica colorida 1" />
         </div>
       </Carousel>
     </div>
--- front/src/data/products.js
+++ front/src/data/products.js
@@ -1,36 +1,99 @@
 const products = [
     {
       id: '1',
-      name: 'Figura 1',
-      price: 14990,
-      description: 'Figura de cerámica pintada a mano con tonos suaves.',
+      name: 'Perro Poodle',
+      price: 35000,
+      description: '22x21 cm',
       images: [
-        'https://res.cloudinary.com/dhvxcx2d2/image/upload/f_auto,q_auto,w_600/v1746124988/WhatsApp_Image_2025-05-01_at_14.26.32_jmoh61.jpg',
-        'https://res.cloudinary.com/dhvxcx2d2/image/upload/f_auto,q_auto,w_600/v1746124988/WhatsApp_Image_2025-05-01_at_14.26.32_2_vjn1qc.jpg'
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003702/WhatsApp_Image_2025-06-01_at_20.34.56_ingjh3.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003702/WhatsApp_Image_2025-06-01_at_20.34.56_ingjh3.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003689/WhatsApp_Image_2025-06-01_at_20.34.55_2_zltj9t.jpg'
       ]
     },
     {
       id: '2',
-      name: 'Figura 4',
-      price: 18990,
-      description: 'Obra única pintada a mano con vibrantes colores.',
+      name: 'Perro Poodle',
+      price: 35000,
+      description: '22x21 cm',
       images: [
-        'https://res.cloudinary.com/dhvxcx2d2/image/upload/f_auto,q_auto,w_600/v1746124987/WhatsApp_Image_2025-05-01_at_14.26.33_3_ew9eot.jpg'
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003698/WhatsApp_Image_2025-06-01_at_20.34.57_kuzcbr.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003698/WhatsApp_Image_2025-06-01_at_20.34.57_2_lcvzs9.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003698/WhatsApp_Image_2025-06-01_at_20.34.57_1_f6mike.jpg'
       ]
     },
     {
       id: '3',
-      name: 'Perro salchicha (20x14 cm)',
+      name: 'Elefante',
+      price: 55000,
+      description: '19x33 cm',
+      images: [
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003697/WhatsApp_Image_2025-06-01_at_20.34.52_rjmtkh.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003696/WhatsApp_Image_2025-06-01_at_20.34.52_1_t2sgtk.jpg'
+      ]
+    },
+    {
+      id: '4',
+      name: 'Elefante',
+      price: 55000,
+      description: '19x33 cm',
+      images: [
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003695/WhatsApp_Image_2025-06-01_at_20.34.52_3_vuxfyd.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003694/WhatsApp_Image_2025-06-01_at_20.34.52_2_ju6ctt.jpg'
+      ]
+    },
+    {
+      id: '5',
+      name: 'Elefante',
+      price: 55000,
+      description: '19x33 cm',
+      images: [
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003693/WhatsApp_Image_2025-06-01_at_20.34.53_enonuw.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003693/WhatsApp_Image_2025-06-01_at_20.34.53_1_z5pigr.jpg'
+      ]
+    },
+    {
+      id: '6',
+      name: 'Perro salchicha ',
       price: 29000,
-      description: 'Figura de cerámica pintada a mano con tonos suaves.',
+      description: '20x14 cm',
       images: [
         'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193800/WhatsApp_Image_2025-05-23_at_21.49.04_izlr0a.jpg',
         'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193800/WhatsApp_Image_2025-05-22_at_13.54.11_1_rt6pyj.jpg',
         'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193800/WhatsApp_Image_2025-05-22_at_13.54.11_yzfkvv.jpg',
-        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193800/WhatsApp_Image_2025-05-22_at_13.54.12_1_cwn5ga.jpg',
-        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193803/WhatsApp_Image_2025-05-23_at_13.06.21_2_mueo8c.jpg',
-        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193804/WhatsApp_Image_2025-05-23_at_13.06.21_jn6wtg.jpg',
-        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193803/WhatsApp_Image_2025-05-23_at_13.06.21_1_wouiuu.jpg'
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1748193800/WhatsApp_Image_2025-05-22_at_13.54.12_1_cwn5ga.jpg'
+      ]
+    },
+    {
+      id: '7',
+      name: 'Perro salchicha ',
+      price: 29000,
+      description: '20x14 cm',
+      images: [
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003701/WhatsApp_Image_2025-06-01_at_20.34.56_1_qjk54s.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003700/WhatsApp_Image_2025-06-01_at_20.34.56_2_h6tyxq.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003699/WhatsApp_Image_2025-06-01_at_20.34.56_3_vh8p3t.jpg'
+      ]
+    },
+    {
+      id: '8',
+      name: 'Corazón',
+      price: 25000,
+      description: '15x15 cm',
+      images: [
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003693/WhatsApp_Image_2025-06-01_at_20.34.53_2_ysep4f.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003692/WhatsApp_Image_2025-06-01_at_20.34.54_ducoqj.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003692/WhatsApp_Image_2025-06-01_at_20.34.53_3_htkxr2.jpg'
+      ]
+    },
+    {
+      id: '9',
+      name: 'Corazón',
+      price: 25000,
+      description: '15x15 cm',
+      images: [
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003691/WhatsApp_Image_2025-06-01_at_20.34.54_3_mohweq.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003689/WhatsApp_Image_2025-06-01_at_20.34.54_1_uqocji.jpg',
+        'https://res.cloudinary.com/dhvxcx2d2/image/upload/v1749003689/WhatsApp_Image_2025-06-01_at_20.34.54_2_gyqpxb.jpg'
       ]
     },
   ];
